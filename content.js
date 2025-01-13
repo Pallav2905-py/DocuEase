a59f49c721a79ec4ae5183daadddafc10d292ae1
pallav <pallav.dev@outlook.com>
2025-01-13 18:56:14 +0530

    Switched to React For FrontEnd

--- content.js
+++
@@ -1,7 +0,0 @@
-// content.js
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.action === "scrape") {
-        const pageContent = document.body.innerText;
-        sendResponse({ content: pageContent });
-    }
-});
